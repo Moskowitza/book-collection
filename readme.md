a0421d0465c8be480d0883ae4541b5c76e26d665
moskowitza <moskowitzaaron@mac.com>
2019-07-17 10:43:28 -0400

    getting winston and morgan working

--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
 ## Typescript Express Postgres React
+
+Getting started:
+Install typescript (and node & npm) globally.
+Clone this repo and run `npm install`.
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,48 @@
 import express from 'express';
+import morgan from 'morgan';
+import path from 'path';
+import winston from 'winston';
 const app = express();
 const port = 8080; // default port to listen
 
+const logger = winston.createLogger({
+  defaultMeta: { service: 'user-service' },
+  format: winston.format.json(),
+  level: 'info',
+  transports: [
+    new winston.transports.File({
+      filename: '../logs/error.log',
+      level: 'error',
+    }),
+    new winston.transports.File({ filename: '../logs/combined.log' }),
+  ],
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    })
+  );
+}
+class MyStream {
+  public write(text: string) {
+    logger.info(text);
+  }
+}
+app.use(morgan('combined', { stream: new MyStream() }));
+
+// Configure Express to use EJS
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 // define a route handler for the default home page
 app.get('/', (req, res) => {
-  res.send('hello world');
+  // render the index template
+  res.render('index');
 });
 
 // start the Express server
 app.listen(port, () => {
-  // tslint:disable-next-line:no-console
-  console.log(`server started at http://localhost:${port}`);
+  winston.log('info', `server started at http://localhost:${port}`);
 });
